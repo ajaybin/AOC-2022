827dfb55ef199f8a18b75310bae80fb878993f3a
Ajay Bindingnavale <ajaybin@users.noreply.github.com>
2022-12-09 23:44:09 -0600

    Update Source.cpp

--- D10/Source.cpp
+++ D10/Source.cpp
@@ -4,34 +4,32 @@
 #include <regex>
 using namespace std;
 
+int cycle = 1, next_cycle = 2, total = 0, weigth = 20, X = 1;
+string s;
+
+void shift() {
+	if (cycle == weigth) {
+		total += weigth * X;
+		weigth += 40;
+	}
+	s.push_back(((cycle - 1) % 40 >= X - 1 && (cycle - 1) % 40 <= X + 1) ? '#' : '.');
+	cycle = next_cycle;
+	next_cycle++;
+}
+
 int main() {
 	ifstream infile("Input.txt");
 	string line;
-	
-	int cycle = 1, next_cycle = 2, total = 0, weigth = 20, X = 1;
-	string s;
 
 	while (getline(infile, line)) {
 		smatch sm;
 		if (regex_match(line, regex("noop"))) {
-			if (cycle == weigth) {
-				total += weigth * X;
-				weigth += 40;
-			}
-			s.push_back(((cycle-1)%40 >= X - 1 && (cycle-1)%40 <= X + 1) ? '#' :  '.');
-			cycle = next_cycle;
-			next_cycle++;
+			shift();
 		}
 		else if (regex_match(line, sm, regex("addx (-?\\d+)"))) {
 			//cout << stoi(string(sm[1])) << endl;
 			for (int i = 0; i < 2; i++) {
-				if (cycle == weigth) {
-					total += weigth * X;
-					weigth += 40;
-				}
-				s.push_back(((cycle - 1) % 40 >= X - 1 && (cycle - 1) % 40 <= X + 1) ? '#' : '.');
-				cycle = next_cycle;
-				next_cycle++;
+				shift();
 			}
 			X += stoi(string(sm[1]));
 		}
