7e14726beec9e03ab4669ae6c795289b1ee0b836
Ajay Bindingnavale <ajaybin@users.noreply.github.com>
2022-12-10 12:50:05 -0600

    Update Source.cpp

--- D9/P2/Source.cpp
+++ D9/P2/Source.cpp
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+map<pair<int, int>, bool> mymap;
 class position {
 public:
 	int x, y;
@@ -75,6 +76,16 @@ void movetail(position& head, position& tail) {
 
 }
 
+void do_move(position &head, position knots[9]) {
+	pair<int, int> key(0, 0);
+	movetail(head, knots[0]);
+	for (int i = 0; i < 8; i++) {
+		movetail(knots[i], knots[i + 1]);
+	}
+	key = std::make_pair(knots[8].x, knots[8].y);
+	mymap[key] = true;
+}
+
 int main() {
 	ifstream infile("Input.txt");
 	string line;
@@ -82,12 +93,8 @@ int main() {
 	position head;
 	position knots[9]; // knots 0-8 with knot 8 as the tail
 
-	pair<int, int> key(0, 0);
-	map<pair<int, int>, bool> mymap;
+	mymap[{0,0}] = true;
 
-	mymap[key] = true;
-
-	//SO UGLY. DAMN I need to clean this up(TM)
 	while (getline(infile, line)) {
 		smatch sm;
 		regex_match(line, sm, regex("([A-Z]) (\\d+)"));
@@ -95,45 +102,25 @@ int main() {
 		if (sm[1] == "U") {
 			for (int i = 0; i < movement; i++) {
 				head.y += 1;
-				movetail(head, knots[0]);
-				for (int i = 0; i < 8; i++) {
-					movetail(knots[i], knots[i + 1]);
-				}
-				key = std::make_pair(knots[8].x, knots[8].y);
-				mymap[key] = true;
+				do_move(head, knots);
 			}
 		}
 		else if (sm[1] == "D") {
 			for (int i = 0; i < movement; i++) {
 				head.y -= 1;
-				movetail(head, knots[0]);
-				for (int i = 0; i < 8; i++) {
-					movetail(knots[i], knots[i + 1]);
-				}
-				key = std::make_pair(knots[8].x, knots[8].y);
-				mymap[key] = true;
+				do_move(head, knots);
 			}
 		}
 		else if (sm[1] == "L") {
 			for (int i = 0; i < movement; i++) {
 				head.x -= 1;
-				movetail(head, knots[0]);
-				for (int i = 0; i < 8; i++) {
-					movetail(knots[i], knots[i + 1]);
-				}
-				key = std::make_pair(knots[8].x, knots[8].y);
-				mymap[key] = true;
+				do_move(head, knots);
 			}
 		}
 		else if (sm[1] == "R") {
 			for (int i = 0; i < movement; i++) {
 				head.x += 1;
-				movetail(head, knots[0]);
-				for (int i = 0; i < 8; i++) {
-					movetail(knots[i], knots[i + 1]);
-				}
-				key = std::make_pair(knots[8].x, knots[8].y);
-				mymap[key] = true;
+				do_move(head, knots);
 			}
 		}
 	}
